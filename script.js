49c5b8cd19b4306102ce4b9b8a8aeb6f1c02d900
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-06-06 14:07:44 -0300

    feat: adicao da logica dadicionar habitos e criar os elementos html dinamicamente

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 const input= document.getElementById("habit-input");
-const botao= document.getElementById("add-btn");
-const sessaoHabitList= document.getElementById("habit-list");
-const habitCard= document.querySelector(".habit-card");
-const completeBtn= document.querySelector(".complete-btn");
-const editBtn= document.querySelector(".edit-btn");
-const deleteBtn= document.querySelector(".delete-btn");
+const addButton= document.getElementById("add-btn");
+const sectionHabitList= document.getElementById("habit-list");
+let allHabit= [];
 
-botao.addEventListener("click", ()=>{
-    if(input.value === ""){
-        alert("adicione um habito no campo abaixo.");
-    }
-    habitCard.append(input.value);
-})
+
+addButton.addEventListener("click", ()=>{
+    const newHabit= {
+        id: Date.now(),
+        nome: input.value,
+        concluido: false
+    };
+
+    if(input.value == ""){
+        return;
+    };
+
+    const value= input.value;
+
+    const habitCard= document.createElement("div");
+    habitCard.classList.add("habit-card");
+
+    const habitInfo= document.createElement("h2");
+    habitInfo.classList.add("habit-info");
+    habitInfo.textContent= value;
+
+    const habitActions= document.createElement("div");
+    habitActions.classList.add("habit-actions");
+
+    const completeBtn= document.createElement("button");
+    completeBtn.classList.add("complete-btn");
+    completeBtn.textContent= "Concluir";
+
+    const editBtn= document.createElement("button");
+    editBtn.classList.add("edit-btn");
+    editBtn.textContent= "Editar";
+
+    const deleteBtn= document.createElement("button");
+    editBtn.classList.add("delete-btn");
+    deleteBtn.textContent= "Excluir";
+
+    habitActions.append(completeBtn);
+    habitActions.append(editBtn);
+    habitActions.append(deleteBtn);
+
+
+    habitCard.append(habitInfo);
+    habitCard.append(habitActions)
+
+    sectionHabitList.append(habitCard);
+    // sectionHabitList.append(habitActions);
+
+    allHabit.push(newHabit);
+    console.log(allHabit)
+
+    input.value= "";
+    localStorage.setItem("habits", JSON.stringify(allHabit));
+});
