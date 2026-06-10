65de54cdbbe6a02d5721f5806e2971df6c4faad1
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-06-10 14:08:07 -0300

    feat: conclusao das logicas dos botoes

--- script.js
+++ script.js
@@ -1,58 +1,137 @@
-const input= document.getElementById("habit-input");
-const addButton= document.getElementById("add-btn");
-const sectionHabitList= document.getElementById("habit-list");
-let allHabit= [];
+const input = document.getElementById("habit-input");
+const addButton = document.getElementById("add-btn");
+const sectionHabitList = document.getElementById("habit-list");
 
+// cria um array no localStorage
+let allHabit = JSON.parse(localStorage.getItem("habits")) || [];
 
-addButton.addEventListener("click", ()=>{
-    const newHabit= {
-        id: Date.now(),
-        nome: input.value,
-        concluido: false
+// criamos a função para adicionar habitos no array , e chamamos a função para renderizar os habitos na tela
+function addHabit() {
+    if(!input.value.trim()){
+      return
     };
 
-    if(input.value == ""){
+  const newHabit = {
+    id: Date.now(),
+    nome: input.value,
+    concluido: false,
+  };
+
+  allHabit.push(newHabit);
+
+  localStorage.setItem("habits", JSON.stringify(allHabit));
+  renderHabit(newHabit);
+
+  input.value = "";
+}
+
+// função para renderizar os habitos na tela
+function renderHabit(habit) {
+  const habitCard = document.createElement("div");
+  habitCard.classList.add("habit-card");
+
+  const habitInfo = document.createElement("div");
+  habitInfo.classList.add("habit-info");
+
+  const habitTittle = document.createElement("h2");
+  habitTittle.textContent = habit.nome;
+
+  // create actions buttons
+  const habitActions = document.createElement("div");
+  habitActions.classList.add("habit-actions");
+
+  const buttonComplete = document.createElement("button");
+  buttonComplete.classList.add("complete-btn");
+  buttonComplete.textContent = "Concluir";
+
+    buttonComplete.addEventListener("click", ()=>{
+        completeHabit(habit.id);
+    });
+
+  const buttonEdit = document.createElement("button");
+  buttonEdit.classList.add("edit-btn");
+  buttonEdit.textContent = "Editar";
+  
+    buttonEdit.addEventListener("click", () => {
+    editHabit(habit.id);
+    });
+
+  const buttonDelete = document.createElement("button");
+  buttonDelete.classList.add("delete-btn");
+  buttonDelete.textContent = "Excluir";
+
+    buttonDelete.addEventListener("click", ()=>{
+        excludeHabit(habit.id);
+    })
+
+  habitCard.append(habitInfo, habitActions);
+  habitInfo.append(habitTittle);
+
+  habitActions.append(buttonComplete, buttonEdit, buttonDelete);
+
+  sectionHabitList.append(habitCard);
+}
+
+// aqui criamos a função para recuperar os habitos salvos na localStorage e trazer de volta para a tela
+function loadHabits() {
+  allHabit.forEach((habit) => {
+    renderHabit(habit);
+  });
+}
+
+loadHabits();
+
+addButton.addEventListener("click", addHabit);
+
+// logic of button edit
+
+function editHabit(id) {
+    const newName = prompt("What is the new name for the habit?");
+
+    if (!newName) {
         return;
-    };
+    }
 
-    const value= input.value;
+    const habit = allHabit.find(h => h.id === id);
 
-    const habitCard= document.createElement("div");
-    habitCard.classList.add("habit-card");
+    if (habit) {
+        habit.nome = newName;
+    }
 
-    const habitInfo= document.createElement("h2");
-    habitInfo.classList.add("habit-info");
-    habitInfo.textContent= value;
+    localStorage.setItem(
+        "habits",
+        JSON.stringify(allHabit)
+    );
 
-    const habitActions= document.createElement("div");
-    habitActions.classList.add("habit-actions");
+    sectionHabitList.innerHTML = "";
 
-    const completeBtn= document.createElement("button");
-    completeBtn.classList.add("complete-btn");
-    completeBtn.textContent= "Concluir";
+    allHabit.forEach(habit => {
+        renderHabit(habit);
+    });
+}
 
-    const editBtn= document.createElement("button");
-    editBtn.classList.add("edit-btn");
-    editBtn.textContent= "Editar";
+function excludeHabit(id){
+    allHabit= allHabit.filter(h => h.id !== id);
 
-    const deleteBtn= document.createElement("button");
-    editBtn.classList.add("delete-btn");
-    deleteBtn.textContent= "Excluir";
-
-    habitActions.append(completeBtn);
-    habitActions.append(editBtn);
-    habitActions.append(deleteBtn);
-
-
-    habitCard.append(habitInfo);
-    habitCard.append(habitActions)
-
-    sectionHabitList.append(habitCard);
-    // sectionHabitList.append(habitActions);
-
-    allHabit.push(newHabit);
-    console.log(allHabit)
-
-    input.value= "";
     localStorage.setItem("habits", JSON.stringify(allHabit));
-});
+
+    sectionHabitList.innerHTML= "";
+
+    allHabit.forEach(habit =>{
+        renderHabit(habit)
+    });
+}
+
+function completeHabit(id){
+    const habit = allHabit.find(h => h.id === id);
+
+    habit.concluido= !habit.concluido;
+
+    localStorage.setItem("habits", JSON.stringify(allHabit));
+
+    sectionHabitList.innerHTML = "";
+
+    allHabit.forEach(habit => {
+        renderHabit(habit);
+    });
+}
